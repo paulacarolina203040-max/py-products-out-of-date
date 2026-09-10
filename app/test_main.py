5dc5610bdf896e5585d90b80762278518d43ff07
paulacarolina203040-max <paulacarolina203040@gmail.com>
2026-09-10 16:38:44 -0300

    Products out

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,30 @@
-# write your code here
+import datetime
+from unittest.mock import patch
+
+from app.main import outdated_products
+
+
+@patch("app.main.datetime.date")
+def test_outdated_products(mock_date: object) -> None:
+    mock_date.today.return_value = datetime.date(2022, 2, 2)
+
+    products = [
+        {
+            "name": "salmon",
+            "expiration_date": datetime.date(2022, 2, 10),
+            "price": 600,
+        },
+        {
+            "name": "chicken",
+            "expiration_date": datetime.date(2022, 2, 5),
+            "price": 120,
+        },
+        {
+            "name": "duck",
+            "expiration_date": datetime.date(2022, 2, 1),
+            "price": 160,
+        },
+    ]
+
+    result = outdated_products(products)
+    assert result == ["duck"]
